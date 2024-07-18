001b78b41cff73f4f7ad267646afc4d44c1f7b67
PS75215 <p.saha@zensar.com>
2024-07-19 01:25:12 +0530

    Setting up java project: added Target.txt and Polymorphism.java class

---
+++ com/practice/oopsconcepts/Polymorphism.java
@@ -0,0 +1,10 @@
+package com.practice.oopsconcepts;
+
+public class Polymorphism {
+
+    public static void  main(String[] args){
+
+        System.out.println("Hi Prachurja !!");
+        
+    }
+}
